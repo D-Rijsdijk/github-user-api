da97d317f25b0bf1bcf892479c204395df02df0a
Diana Rijsdijk <diana.rijsdijk09@gmail.com>
2025-03-20 19:06:14 -0400

    Criar serviço para buscar usuário no GitHub

---
+++ services.py
@@ -0,0 +1,21 @@
+import requests
+
+def get_github_user(username):
+    if not username: 
+        return {'error': 'Usário não informado'}, 400 
+    
+    url = f'https://api.github.com/users/{username}'
+    response = requests.get(url)
+
+    if response.status_code == 404:
+        return {'error': 'Usuário não encontrado'}, 404
+    
+    data = response.json()
+    return{
+        'nome': data.get('name'),
+        'avatar_url': data.get('avatar_url'),
+        'email': data.get('email'),
+        'bio': data.get('bio'),
+        'seguidores': data.get('followers'),
+        'quantidade_repositorios': data.get('public_repos'),
+    }, 200
